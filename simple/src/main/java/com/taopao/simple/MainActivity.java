5d02b789bc183eec23f190903fe0eae164601f64
shenkun <825210484@qq.com>
2018-11-16 17:12:20 +0800

    封装库调用so

---
+++ simple/src/main/java/com/taopao/simple/MainActivity.java
@@ -0,0 +1,32 @@
+package com.taopao.simple;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.taopao.androidnginxrtmp.service.NginxService;
+
+import org.screenshare.rtmp.nginx.Nginx;
+
+public class MainActivity extends AppCompatActivity {
+//    Nginx mNginx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        mNginx = Nginx.create();
+        findViewById(R.id.open).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                mNginx.start();
+                Intent intent = new Intent(MainActivity.this, NginxService.class);
+                intent.setAction(NginxService.ACTION_START_NGINX);
+                startService(intent);
+            }
+        });
+
+
+    }
+}
